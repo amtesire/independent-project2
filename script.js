fd11ecd9085755cbbdee8ac87c426719229ba453
evelyne250 <uevelyne44@gmail.com>
2019-08-01 10:07:51 +0200

    added more buttons

---
+++ script.js
@@ -0,0 +1,105 @@
+$(document).ready(function(){
+    $(".wor").hide();
+    $("#work").css({
+     opacity:1
+   });
+    $("#wo").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor").fadeOut();
+    });
+  });
+  $(document).ready(function(){
+    $(".wor1").hide();
+    $("#work1").css({
+     opacity:1
+   });
+    $("#wo1").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor1").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor1").fadeOut();
+    });
+  });
+  
+  $(document).ready(function(){
+    $(".wor2").hide();
+    $("#work2").css({
+     opacity:1
+   });
+    $("#wo2").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor2").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor2").fadeOut();
+    });
+  });
+  $(document).ready(function(){
+    $(".wor3").hide();
+    $("#work3").css({
+     opacity:1
+   });
+    $("#wo3").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor3").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor3").fadeOut();
+    });
+  });
+  $(document).ready(function(){
+    $(".wor4").hide();
+    $("#work4").css({
+     opacity:1
+   });
+    $("#wo4").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor4").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor4").fadeOut();
+    });
+  });
+  $(document).ready(function(){
+    $(".wor5").hide();
+    $("#work5").css({
+     opacity:1
+   });
+    $("#wo5").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor5").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor5").fadeOut();
+    });
+  });
+  $(document).ready(function(){
+    $(".wor6").hide();
+    $("#work6").css({
+     opacity:1
+   });
+    $("#wo6").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor6").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor6").fadeOut();
+    });
+  });
+  $(document).ready(function(){
+    $(".wor7").hide();
+    $("#work7").css({
+     opacity:1
+   });
+    $("#wo7").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor7").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor7").fadeOut();
+    });
+  });

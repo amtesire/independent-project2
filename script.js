536864056b3ecb608d62aca041f4e27860b8771f
evelyne250 <uevelyne44@gmail.com>
2019-08-05 16:35:19 +0200

    modified for the final touches

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-document.getElementById("delivery").addEventListener("click", function () {
-  prompt("enter your location");
-  alert("thank you, your order will be processed and get back at you");
-})
-
 // business logic
+$(document).ready(function(){
 function Pizza(size, crust, topping, quantity, totalprice){
   this.size = size;
   this.crust = crust;
@@ -14,109 +10,109 @@ function Pizza(size, crust, topping, quantity, totalprice){
 Pizza.prototype.fullOrder = function() {
   return this.size + " " + this.crust + " " +this.topping + " " + this.quantity + " " + this.totalprice;
 }
-// $(document).ready(function(){
-//     $(".wor").hide();
-//     $("#work").css({
-//      opacity:1
-//    });
-//     $("#wo").hover(function(){
-//       $(this).stop().css({opacity:.4});
-//       $(".wor").fadeIn();
-//       }, function(){
-//         $(this).stop().css({opacity:1})
-//       $(".wor").fadeOut();
-//     });
-//   });
-//   $(document).ready(function(){
-//     $(".wor1").hide();
-//     $("#work1").css({
-//      opacity:1
-//    });
-//     $("#wo1").hover(function(){
-//       $(this).stop().css({opacity:.4});
-//       $(".wor1").fadeIn();
-//       }, function(){
-//         $(this).stop().css({opacity:1})
-//       $(".wor1").fadeOut();
-//     });
+$(document).ready(function(){
+    $(".wor").hide();
+    $("#work").css({
+     opacity:1
+   });
+    $("#wo").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor").fadeOut();
+    });
+  });
+  $(document).ready(function(){
+    $(".wor1").hide();
+    $("#work1").css({
+     opacity:1
+   });
+    $("#wo1").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor1").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor1").fadeOut();
+    });
   
-//     $(".wor2").hide();
-//     $("#work2").css({
-//      opacity:1
-//    });
-//     $("#wo2").hover(function(){
-//       $(this).stop().css({opacity:.4});
-//       $(".wor2").fadeIn();
-//       }, function(){
-//         $(this).stop().css({opacity:1})
-//       $(".wor2").fadeOut();
-//     });
-//   });
-//   $(document).ready(function(){
-//     $(".wor3").hide();
-//     $("#work3").css({
-//      opacity:1
-//    });
-//     $("#wo3").hover(function(){
-//       $(this).stop().css({opacity:.4});
-//       $(".wor3").fadeIn();
-//       }, function(){
-//         $(this).stop().css({opacity:1})
-//       $(".wor3").fadeOut();
-//     });
-//   });
-//   $(document).ready(function(){
-//     $(".wor4").hide();
-//     $("#work4").css({
-//      opacity:1
-//    });
-//     $("#wo4").hover(function(){
-//       $(this).stop().css({opacity:.4});
-//       $(".wor4").fadeIn();
-//       }, function(){
-//         $(this).stop().css({opacity:1})
-//       $(".wor4").fadeOut();
-//     });
-//   });
-//   $(document).ready(function(){
-//     $(".wor5").hide();
-//     $("#work5").css({
-//      opacity:1
-//    });
-//     $("#wo5").hover(function(){
-//       $(this).stop().css({opacity:.4});
-//       $(".wor5").fadeIn();
-//       }, function(){
-//         $(this).stop().css({opacity:1})
-//       $(".wor5").fadeOut();
-//     });
-//   });
-//   $(document).ready(function(){
-//     $(".wor6").hide();
-//     $("#work6").css({
-//      opacity:1
-//    });
-//     $("#wo6").hover(function(){
-//       $(this).stop().css({opacity:.4});
-//       $(".wor6").fadeIn();
-//       }, function(){
-//         $(this).stop().css({opacity:1})
-//       $(".wor6").fadeOut();
-//     });
-//   });
-//   $(document).ready(function(){
-//     $(".wor7").hide();
-//     $("#work7").css({
-//      opacity:1
-//    });
-//     $("#wo7").hover(function(){
-//       $(this).stop().css({opacity:.4});
-//       $(".wor7").fadeIn();
-//       }, function(){
-//         $(this).stop().css({opacity:1})
-//       $(".wor7").fadeOut();
-//     });
-//   });
+    $(".wor2").hide();
+    $("#work2").css({
+     opacity:1
+   });
+    $("#wo2").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor2").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor2").fadeOut();
+    });
+  });
+  $(document).ready(function(){
+    $(".wor3").hide();
+    $("#work3").css({
+     opacity:1
+   });
+    $("#wo3").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor3").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor3").fadeOut();
+    });
+  });
+  $(document).ready(function(){
+    $(".wor4").hide();
+    $("#work4").css({
+     opacity:1
+   });
+    $("#wo4").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor4").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor4").fadeOut();
+    });
+  });
+  $(document).ready(function(){
+    $(".wor5").hide();
+    $("#work5").css({
+     opacity:1
+   });
+    $("#wo5").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor5").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor5").fadeOut();
+    });
+  });
+  $(document).ready(function(){
+    $(".wor6").hide();
+    $("#work6").css({
+     opacity:1
+   });
+    $("#wo6").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor6").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor6").fadeOut();
+    });
+  });
+  $(document).ready(function(){
+    $(".wor7").hide();
+    $("#work7").css({
+     opacity:1
+   });
+    $("#wo7").hover(function(){
+      $(this).stop().css({opacity:.4});
+      $(".wor7").fadeIn();
+      }, function(){
+        $(this).stop().css({opacity:1})
+      $(".wor7").fadeOut();
+    });
+  });
 
 // user interface
 $(document).ready(function(){
@@ -155,21 +151,21 @@ $(document).ready(function(){
     if(crustd = "thin") {
       totalprice = (price*numberf) + 250;
     }
-    // else if (crustd = "thick"){
-    //   totalprice = (price*numberf)+ 250;
-    // }
-    // else if (crustd = "flatbread"){
-    //   totalprice = (price*numberf) + 250;
-    // }
-    // else if (crustd = "custom crusts"){
-    //   totalprice = (price*numberf) + 250;
-    // }
-    // else if (crustd = "Authentic Wood Fired Crusts"){
-    //   totalprice = (price*numberf) + 250;
-    // }
-    // else if (crustd = "focaccia"){
-    //   totalprice = (price*numberf) + 250;
-    // }
+    else if (crustd = "thick"){
+      totalprice = (price*numberf)+ 250;
+    }
+    else if (crustd = "flatbread"){
+      totalprice = (price*numberf) + 250;
+    }
+    else if (crustd = "custom crusts"){
+      totalprice = (price*numberf) + 250;
+    }
+    else if (crustd = "Authentic Wood Fired Crusts"){
+      totalprice = (price*numberf) + 250;
+    }
+    else if (crustd = "focaccia"){
+      totalprice = (price*numberf) + 250;
+    }
     break;
     
     case size = "medium":
@@ -251,9 +247,13 @@ $(document).ready(function(){
 
 });
 
+$("#delivery").click(function(){
+  
+ var location = prompt("enter your location");
+  alert("thank you, your order will be processed and be delivered to you in 1 hour at "+location );
+});
 
-
-
+});
 
 
 
